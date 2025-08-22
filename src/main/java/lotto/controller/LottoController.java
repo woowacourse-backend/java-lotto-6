a18921b36ab36a7becf4e8ec267b0a66a0ae8199
ParkJunSeok <pjh203203@naver.com>
2025-08-22 23:27:51 +0900

    refactor: 로또 구입금액 검증로직 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,8 +1,14 @@
 package lotto.controller;
 
+import lotto.validator.AmountValidator;
 import lotto.view.InputView;
 
 public class LottoController {
+    private final AmountValidator validator;
+
+    public LottoController() {
+        this.validator = new AmountValidator();
+    }
 
     public void run() {
         int buyAmount = getAmount();
@@ -11,11 +17,11 @@ public class LottoController {
     }
 
 
-    private int getAmount() {
+    public int getAmount() {
         while (true) {
             try {
                 String input = InputView.buyAmountInput();
-                return validateAmount(input);
+                return validator.validateAmount(input);
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
@@ -23,45 +29,6 @@ public class LottoController {
 
     }
 
-    private int validateAmount(String input) {
-        // 공백이나 빈값인지 검증
-        validateEmptyNumber(input);
-        // 숫자인지 검증
-        int amount = validateNumber(input);
 
-        // 양수인지 검증
-        validateNegativeNumber(amount);
-        // 1000원 단위로 나누어 떨어지는지 검증
-        validateNumberUnit(amount);
-
-        return amount;
-    }
-
-    private void validateEmptyNumber(String input) {
-        if (input.trim().isEmpty()) {
-            throw new IllegalArgumentException("[ERROR] 구입 금액은 공백이나 빈값은 입력할 수 없습니다.");
-        }
-    }
-
-    public int validateNumber(String input) {
-        boolean result = input.matches("-?[0-9]+");
-        if (!result) {
-            throw new IllegalArgumentException("[ERROR] 구입 금액은 문자는 입력할 수 없습니다.");
-        }
-
-        return Integer.parseInt(input);
-    }
-
-    public void validateNegativeNumber(int amount) {
-        if (amount < 0) {
-            throw new IllegalArgumentException("[ERROR] 구입 금액은 음수값은 입력할 수 없습니다.");
-        }
-    }
-
-    private void validateNumberUnit(int amount) {
-        if (amount % 1000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 나누어 떨어져야합니다.");
-        }
-    }
 
 }
