e1927db7e80d2d492ebe290f522daf1511527959
ParkJunSeok <pjh203203@naver.com>
2025-08-23 13:14:15 +0900

    fix: 구입금액 출력 부분 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public static String buyAmountInput() {
-        System.out.print("구입금액을 입력해 주세요.");
+        System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
 
     }
