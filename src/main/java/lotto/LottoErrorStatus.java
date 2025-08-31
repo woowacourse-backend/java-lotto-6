a73ac9a866d3c371f4e29b34130eaf4305467a9b
bee9827 <ehfrhfo9494@naver.com>
2025-08-31 17:38:10 +0900

    refactor: LottoErrorStatus 출력 문구 변경

--- src/main/java/lotto/LottoErrorStatus.java
+++ src/main/java/lotto/LottoErrorStatus.java
@@ -1,10 +1,10 @@
 package lotto;
 
 public enum LottoErrorStatus {
-    INVALID_SIZE("로또 숫자의 개수가 잘못 되었습니다."),
-    INVALID_RANGE("로또 숫자의 범위가 잘못 되었습니다."),
+    INVALID_SIZE("로또 번호의 개수는 %d개 여야 합니다.".formatted(Lotto.SIZE)),
+    INVALID_RANGE("로또 번호는 %d부터 %d사이의 숫자여야 합니다.".formatted(Lotto.MIN_VALUE, Lotto.MAX_VALUE)),
     DUPLICATE("로또 번호는 중복 될 수 없습니다."),
-    INVALID_RANGE_BONUS_NUMBER("보너스 번호의 범위가 잘못 되었습니다."),
+    INVALID_RANGE_BONUS_NUMBER("보너스 번호는 %d 부터 %d사이의 숫자여야 합니다.".formatted(Lotto.MIN_VALUE, Lotto.MAX_VALUE)),
     DUPLICATE_BONUS_NUMBER("보너스 번호는 중복 될 수 없습니다."),
     ;
 
